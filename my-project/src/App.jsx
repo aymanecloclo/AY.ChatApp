c5dc4e44c0ead038cca86860cbac5d3f1780bdfb
aymane <aymanerachid47@gmail.com>
2024-11-21 17:03:36 +0100

    first project

---
+++ my-project/src/App.jsx
@@ -0,0 +1,11 @@
+import Register from "./components/Register"
+import Navbar from "./components/Navbar"
+export default function App() {
+  return (
+    <>
+          <Navbar/>
+          <Register/>
+    </>
+
+  );
+}
